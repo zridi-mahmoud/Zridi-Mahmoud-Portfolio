17808a2959390490da107260d951df4d0e7715d6
mahmoud <mahmoudzridi@gmail.com>
2021-03-31 16:24:52 +0100

    create aboute me

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,72 @@
-import React from 'react';
+import React from "react";
 import logo from "../logo.png";
 //REACT FONTAWSOME IMPORT
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container">
-                <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="mahmoud"/></a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
-                </button>
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light navbar-custom">
+        <div className="container">
+          {/* <a className="navbar-brand" href="#">
+            <img className="logo" src={logo} alt="mahmoud" />
+          </a> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
+          </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">About me</a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Skills <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Education <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Experience <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Portfolio <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Contact <span className="sr-only">(current)</span></a>
-                    </li>
-                    </ul>
-                </div>
-            </div>
-            </nav>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#aboutMe">
+                  About me
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Skills <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Education <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Experience <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Portfolio <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Contact <span className="sr-only">(current)</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
